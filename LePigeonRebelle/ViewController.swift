6a4b752b54f3d888f531546220b51131be898f46
Walid Sadaoui <walidsadaoui@hotmail.fr>
2018-01-01 21:13:07 +0100

    Xcode Project Setup

---
+++ LePigeonRebelle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LePigeonRebelle
+//
+//  Created by iMac on 01/01/2018.
+//  Copyright © 2018 Le Pigeon Rebelle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
